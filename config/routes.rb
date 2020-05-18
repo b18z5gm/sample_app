e1789361e193b9860e3290134a41553e33be13b4
Ubuntu <ubuntu@ip-172-31-35-104.ap-northeast-1.compute.internal>
2020-05-18 13:48:52 +0000

    update with 'contact'

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   
   get 'static_pages/about'
   
+  
+  get 'static_pages/contact'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 
