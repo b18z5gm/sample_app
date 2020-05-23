94fa00655ee589bd61794bcbb5bc15afd1bc6c0c
Ubuntu <ubuntu@ip-172-31-35-104.ap-northeast-1.compute.internal>
2020-05-23 03:53:31 +0000

    finish ch05

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,29 @@
 Rails.application.routes.draw do
+  # get 'users/new'
   root 'static_pages#home'
-  # root 'application#hello'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+end
+
+# Rails.application.routes.draw do
+  # get 'users/new'
+#   root 'static_pages#home'
+#   # root 'application#hello'
   
-  get 'static_pages/home'
-  # => StaticPages#home
+#   get 'static_pages/home'
+#   # => StaticPages#home
   
-  get 'static_pages/help'
-  # => StaticPages#help
+#   get 'static_pages/help'
+#   # => StaticPages#help
   
-  get 'static_pages/about'
+#   get 'static_pages/about'
   
   
-  get 'static_pages/contact'
+#   get 'static_pages/contact'
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 
-end
+# end
